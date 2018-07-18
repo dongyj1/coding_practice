96db633b70bf68b5b4f618b60ac395672617eacd
dongyj1 <dongyj@bu.edu>
2018-07-18 01:20:39 -0400

    ListNode

---
+++ src/solutions/ListNode.java
@@ -0,0 +1,10 @@
+package solutions;
+
+/**
+ * Created by dyj on 7/18/18.
+ */
+public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+}
