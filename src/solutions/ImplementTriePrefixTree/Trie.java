3293228a954aa5355619fd36048b250ffa058443
dongyj1 <dongyj@bu.edu>
2018-07-26 00:51:20 -0400

    implement trie

---
+++ src/solutions/ImplementTriePrefixTree/Trie.java
@@ -0,0 +1,63 @@
+package solutions.ImplementTriePrefixTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dyj on 7/26/18.
+ */
+public class Trie {
+    TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] == null) {
+                cur.children[c - 'a'] = new TrieNode();
+            }
+            cur = cur.children[c - 'a'];
+        }
+        cur.words.add(word);
+        cur.isEnd = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (cur.children[c - 'a'] == null) {
+                return false;
+            }
+            cur = cur.children[c - 'a'];
+        }
+        return cur.isEnd;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            if (cur.children[c - 'a'] == null) {
+                return false;
+            }
+            cur = cur.children[c - 'a'];
+        }
+        return true;
+    }
+
+    class TrieNode {
+        boolean isEnd;
+        TrieNode[] children;
+        List<String> words;
+        TrieNode() {
+            children = new TrieNode[26];
+            words = new ArrayList<>();
+            isEnd = false;
+        }
+    }
+}
