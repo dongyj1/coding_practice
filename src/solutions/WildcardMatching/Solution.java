fcc0565a8a2770d4f66b1295210c0a187d552d84
dongyj1 <dongyj@bu.edu>
2018-08-05 14:57:43 -0400

    wildcard matching

---
+++ src/solutions/WildcardMatching/Solution.java
@@ -0,0 +1,76 @@
+package solutions.WildcardMatching;
+
+/**
+ * Created by dyj on 8/5/18.
+ *
+ Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+ '?' Matches any single character.
+ '*' Matches any sequence of characters (including the empty sequence).
+ The matching should cover the entire input string (not partial).
+
+ Note:
+
+ s could be empty and contains only lowercase letters a-z.
+ p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+ Example 1:
+
+ Input:
+ s = "aa"
+ p = "a"
+ Output: false
+ Explanation: "a" does not match the entire string "aa".
+ Example 2:
+
+ Input:
+ s = "aa"
+ p = "*"
+ Output: true
+ Explanation: '*' matches any sequence.
+ Example 3:
+
+ Input:
+ s = "cb"
+ p = "?a"
+ Output: false
+ Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+ Example 4:
+
+ Input:
+ s = "adceb"
+ p = "*a*b"
+ Output: true
+ Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+ Example 5:
+
+ Input:
+ s = "acdcb"
+ p = "a*c?b"
+ Output: false
+ */
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            char c = p.charAt(i - 1);
+            if (c == '*') {
+                dp[0][i] = dp[0][i - 1];
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+                if (pc == '?' || sc == pc) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - 1] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
